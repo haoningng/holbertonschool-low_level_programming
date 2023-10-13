008fbd5d23ae6f9403cd90efe5c417c365697daf
haoningng <nick.nghn@gmail.com>
2023-10-12 19:33:49 -0700

    add file 101

---
+++ hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Print a sentence followed by a new line
+ *
+ * Return: 1 (if successful)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
