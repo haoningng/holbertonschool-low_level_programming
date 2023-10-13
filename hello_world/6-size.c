837cfada5ea9107d3e5ea3e1d4093313de04f5dd
haoningng <nick.nghn@gmail.com>
2023-10-12 21:47:11 -0700

    update file 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	float f;
 
 	printf("Size of a char: %i byte(s)\n", (int)sizeof(c));
-	printf("Size of a int: %i byte(s)\n", (int)sizeof(i));
+	printf("Size of an int: %i byte(s)\n", (int)sizeof(i));
 	printf("Size of a long int: %i byte(s)\n", (int)sizeof(li));
 	printf("Size of a long long int: %i byte(s)\n", (int)sizeof(lli));
 	printf("Size of a float: %i byte(s)\n", (int)sizeof(f));
