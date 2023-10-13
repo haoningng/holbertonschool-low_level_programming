1e235663b452b58b7bfb6c0e7ebd6e8d36268f9b
haoningng <nick.nghn@gmail.com>
2023-10-12 18:09:17 -0700

    update file 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Prints "Programming is like building a multilingual puzzle
+ *
+ * Return: 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
