807ed8b3ad6fd762dd44cd38963874ddd5611239
haoningng <nick.nghn@gmail.com>
2023-10-12 19:02:49 -0700

    update file 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
 /**
- *
- * main - prints the size of various types on the computer it is compiled and run on
+ * main - prints the size of various types on the computer it's compiled & run
  *
  * Return: 0 if successful
- *
  */
 int main(void)
 {
@@ -14,10 +12,11 @@ int main(void)
 	long int li;
 	long long int lli;
 	float f;
+
 	printf("Size of a char: %i byte(s)\n", (int)sizeof(c));
 	printf("Size of a int: %i byte(s)\n", (int)sizeof(i));
 	printf("Size of a long int: %i byte(s)\n", (int)sizeof(li));
-	printf("Size of a long long int: %i byte(s)\n", (int)sizeof(lii));
+	printf("Size of a long long int: %i byte(s)\n", (int)sizeof(lli));
 	printf("Size of a float: %i byte(s)\n", (int)sizeof(f));
 	return (0);
 }
