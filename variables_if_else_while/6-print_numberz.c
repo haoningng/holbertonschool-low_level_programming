635fe9cc9c22ab545227f343cd4a87c36acc5739
haoningng <nick.nghn@gmail.com>
2023-10-17 04:05:53 -0700

    add fule 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 if succesful
+ */
+int main(void)
+{
+	for (int n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
