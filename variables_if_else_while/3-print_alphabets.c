fb136ae63f3def665db6aa44e626d493826df9e5
haoningng <nick.nghn@gmail.com>
2023-10-17 03:45:58 -0700

    add file 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poitnt
+ *
+ * Return: 0 (Successful)
+ */
+int main(void)
+{
+	char c;
+	int n;
+	int i = 0;
+	char end[] = "\n";
+
+	c = 'a';
+	n = c - 'a';
+	while (n < 26)
+	{
+		putchar(c);
+		c++;
+		n = c - 'a';
+	}
+	d = 'A';
+        n = d - 'A';
+        while (n < 26)
+        {
+                putchar(d);
+                d++;
+                n = d - 'A';
+        }
+	while (i < 1)
+	{
+		putchar(end[i]);
+		i++;
+	}
+
+	return (0);
+}
