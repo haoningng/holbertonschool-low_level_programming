1e3c37fbc27d6d35a86da8c7814e22c92d7bf6aa
haoningng <nick.nghn@gmail.com>
2023-10-18 01:09:51 -0700

    1-alphabet.c

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - print alphabet in lowercase followed by a new line
+ *
+ * Return: none
+ */
+void print_alphabet(void)
+{
+	int alpha = 0;
+
+	while (alpha < 26)
+	{
+		_putchar('a' + alpha);
+		alpha++;
+	}
+	_putchar("\n");
+}
