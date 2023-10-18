45160901c95f1bcdb7b5ec12e104dbed303495d6
haoningng <nick.nghn@gmail.com>
2023-10-18 01:24:29 -0700

    update file 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,16 @@
  */
 void print_alphabet_x10(void)
 {
-	int alpha = 0;
+	int alpha;
 	int time = 0;
 
 	while (time < 10)
 	{
+		alpha = 0;
 		while (alpha < 26)
 		{
 			_putchar('a' + alpha);
+			alpha++;
 		}
 		_putchar('\n');
 	}
