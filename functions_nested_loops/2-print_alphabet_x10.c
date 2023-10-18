d646f47fc86ae6bf63d2f1351f0ccd3176fd8a4d
haoningng <nick.nghn@gmail.com>
2023-10-18 01:31:49 -0700

    update file 2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphebet, in lowercase, followed by a new line
+ * print_alphabet_x10 - prints 10 times the alphebet, in lowercase
  *
  * Return: none
  */
