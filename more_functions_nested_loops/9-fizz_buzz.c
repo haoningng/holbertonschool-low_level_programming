bec152fc036960348ba5a337c57d17bf132235ff
haoningng <nick.nghn@gmail.com>
2023-10-18 19:20:20 -0700

    update file 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 
+void print(int i);
+
 /**
  * main - Entry point
  *
@@ -12,23 +14,40 @@ int main(void)
 
 	for (i = 1; i < 101; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
+		if (i < 100)
+			print();
+			printf(" ");
 		else
 		{
-			printf("%d ", i);
+			print();
 		}
 	}
 	printf("\n");
 	return (0);
+}
+
+/**
+ * print - print the integer
+ * @i: integer to be printed
+ *
+ * Return: none
+ */
+void print(int i)
+{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+		printf("FizzBuzz");
+	}
+	else if (i % 3 == 0)
+	{
+		printf("Fizz");
+	}
+	else if (i % 5 == 0)
+	{
+		printf("Buzz");
+	}
+	else
+	{
+		printf("%d", i);
+	}
 }
