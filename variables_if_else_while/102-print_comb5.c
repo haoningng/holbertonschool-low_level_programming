eb85080a66836efcaf79738e07f676aa4a45ee61
haoningng <nick.nghn@gmail.com>
2023-10-18 20:21:54 -0700

    add file 102

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (if successful)
+ */
+int main(void)
+{
+	int a = 0, b = -1, c = -1, d;
+
+	while (a < 8)
+	{
+		b = a + 1;
+		while (b < 10)
+		{
+			c = b + 1;
+			while (c < 10)
+			{
+				d = c + 1;
+				while (d < 10);
+				{
+					if (b != a && c != a && b != c)
+					{
+						putchar('0' + a);
+						putchar('0' + b);
+						putchar(' ');
+						putchar('0' + c);
+						putchar('0' + d);
+						if (!(a == 9 && b == 8 && c == 9 && d == 9))
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+					d++;
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
