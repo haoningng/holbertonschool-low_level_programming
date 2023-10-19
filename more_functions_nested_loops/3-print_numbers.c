d4672c3bd45cbff74c3846fad6ae915a99937a56
haoningng <nick.nghn@gmail.com>
2023-10-18 17:33:19 -0700

    add file 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9
+ *
+ * Return: none
+ */
+void print_numbers(void)
+{
+	int num = 0;
+	
+	while (num < 10)
+	{
+		_putchar('0' + num);
+		num++;
+	}
+	_putchar('\n');
+}
