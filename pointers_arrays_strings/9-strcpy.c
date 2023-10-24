a5a9338696f2c444879e8b7ef0825350796db767
haoningng <nick.nghn@gmail.com>
2023-10-24 22:26:27 +0000

    add file 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <main.h>
+
+/**
+ * _strcpy - copies the string from src to dest
+ * @dest: destination
+ * @src: source
+ *
+ * Return: Copied string
+ */
+char *_strcpy(char *dest, char *str)
+{
+	strcpy(dest, str);
+}
