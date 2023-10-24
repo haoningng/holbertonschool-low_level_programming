e2f222351d42705fb8d0d7b87283577ec95f5c6f
haoningng <nick.nghn@gmail.com>
2023-10-24 22:09:30 +0000

    update file 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,7 +14,7 @@ void puts_half(char *str)
 	len = strlen(str);
 	if (len % 2 == 0)
 	{
-		for (i = 0; i < len / 2; i++)
+		for (i = len / 2; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
@@ -22,7 +22,7 @@ void puts_half(char *str)
 	else
 	{
 		n = len - 1;
-		for (i = 0; i < n / 2; i++)
+		for (i = n / 2; i < len; i++)
 		{
 			_putchar(str[i]);
 		}
