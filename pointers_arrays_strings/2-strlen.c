97447b6aad974cda12849da1854057ec8323353f
haoningng <nick.nghn@gmail.com>
2023-10-24 11:13:49 +0000

    add file 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be examined
+ *
+ * Return: lenth of a string
+ */
+int _strlen(char *s)
+{
+	return (strlen(*s));
+}
