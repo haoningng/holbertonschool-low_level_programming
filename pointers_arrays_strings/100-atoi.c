75d1161785bf90738e59f53e81cdc49133b6bef2
haoningng <nick.nghn@gmail.com>
2023-10-24 22:50:59 +0000

    add file 100

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string
+ *
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int len, i, j, integer;
+	char *str;
+
+	j = 0;
+	for (i = 0; i < len; i++)
+	{
+		if ((s[i] - 'A') < 27 || (s[i] - 'a') < 27)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		integer = atoi(str);
+	}
+	return (integer);
+}
