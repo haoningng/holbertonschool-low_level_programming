090ce6a7c3226455e7d02d9db56c22d1d28a87a8
haoningng <nick.nghn@gmail.com>
2023-10-25 02:36:13 +0000

    add file 0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - appends the src str to dest str
+ * @dest: destination string
+ * @src: source string
+ *
+ * Return: Resulting destination string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int len_s, len_d, new_len;
+	char *new_string;
+
+	len_d = strlen(dest);
+	len_s = strlen(src);
+	new_len = len_d + len_s;
+	while (*dest)
+	{
+		*new_string = *dest;
+		new_string++;
+		dest++;
+	}
+	while (*src)
+	{
+		*new_string = *src;
+		new_sting++;
+		src++;
+	}
+	*(new_string + new_len) = '0';
+	return (new_string);
+}
