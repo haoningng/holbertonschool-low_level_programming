3469eb6662987388fccdab3e0edf309d302105e3
haoningng <nick.nghn@gmail.com>
2023-10-25 05:38:29 +0000

    add file 4

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * reverse_array - reverses an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: Nothing.
+ */
+void *reverse_array(int *a, int n)
+{
+    int i, len;
+    int new_a[100];
+
+    i = 0;
+    len = sizeof(a) / sizeof(int);
+    while (i < n)
+    {
+        new_a[i] = a[len - 1];
+        i++;
+	len--;
+    }
+}
