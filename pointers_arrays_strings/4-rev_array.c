02591965eb7982b9e0022a5154e7b81ddd51095c
haoningng <nick.nghn@gmail.com>
2023-10-25 05:45:45 +0000

    update file 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -13,14 +13,14 @@ void *reverse_array(int *a, int n)
     int new_a[100];
 
     i = 0;
-    len = (sizeof(a) / sizeof(int));
+    len = n;
     while (i < n)
     {
         new_a[i] = a[len - 1];
         i++;
 	len--;
     }
-    for (i = 0; i < len; i++)
+    for (i = 0; i < n; i++)
     {
     	a[i] = new_a[i];
     }
