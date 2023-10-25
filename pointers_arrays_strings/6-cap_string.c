0d5f59caffebbd8e2d8241c2a84f1d5ddab075e7
haoningng <nick.nghn@gmail.com>
2023-10-25 11:24:42 +0000

    update file 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -24,10 +24,10 @@ char *cap_string(char *str)
 				*(str) -= 32;
 			}
 		}
-		else if (!(isupper(*str) || islower(*str) || (*str == '-'))) 
+		else if (!(isupper(*str) || islower(*str) || (*str == '-')))
 		{
 			i = 0;
-			while (*str) 
+			while (*str)
 			{
 				if (isupper(*(str + i)))
 				{
