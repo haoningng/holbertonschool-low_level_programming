14d112edefe7c02297b7e961b3e84c57ebd3ad43
haoningng <nick.nghn@gmail.com>
2023-10-25 10:58:33 +0000

    update file 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -35,7 +35,7 @@ char *cap_string(char *str)
 				}
 				else if (islower(*(str + i)))
 				{
-					if (atoi(*(str - 1)) >= 0 && atoi(*(str - 1)) <= 9)
+					if (atoi((str - 1)) >= 0 && atoi((str - 1)) <= 9)
 					{
 						break;
 					}
