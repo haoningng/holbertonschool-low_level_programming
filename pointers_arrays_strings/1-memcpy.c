d7a2a46ba2979f62fea28071c6389f4dd679788e
haoningng <nick.nghn@gmail.com>
2023-10-26 05:11:39 +0000

    add file 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+/**
+ * _memcpy - copies n bytes from memory area src to memory area dest
+ * @dest: the destination memory area
+ * @src: the source memory area to be copied from
+ * @n: the number of bytes from memory area to be copied
+ *
+ * Return: a pointer to destination
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i;
+	char *new_str = dest;
+
+	i = 0;
+	while (i < n)
+	{
+		*dest = *src;
+		i++;
+		dest++;
+		src++;
+	}
+	return (new_str);
+}
