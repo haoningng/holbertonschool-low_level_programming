ddf0de70cb0251a1cfab5b02959f883303b2e956
haoningng <nick.nghn@gmail.com>
2023-10-26 01:34:30 +0000

    update file 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,8 @@
 char *cap_string(char *str)
 {
 	int i, cap;
-	char separators[] = {' ', '\t', '\n', ',', ':', '.', '!', '?', '"', '(', ')', '{', '}'};
+	char separators[] = {' ', '\t', '\n', ',', ':', '.', '!', '?',
+		'"', '(', ')', '{', '}'};
 	char *new_str = str;
 
 	cap = 1;
