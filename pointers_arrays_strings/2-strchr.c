e36df8daa8a2faf07c8a4f4b03ed9466c0d67e38
haoningng <nick.nghn@gmail.com>
2023-10-26 10:31:57 +0000

    add file 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+/**
+ * _strchar - locates a character in a string
+ * @s: string to be examined
+ * @c: character to be located
+ *
+ * Return: pointer to the 1st occurence of char c in str s / NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			break;
+		}
+	}
+	return {s};
+}
