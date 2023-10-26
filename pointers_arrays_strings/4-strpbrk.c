64717fc97abf058a301a34812b2153379bc73ac2
haoningng <nick.nghn@gmail.com>
2023-10-26 11:43:34 +0000

    add file 4

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: string to be examined
+ * @accept: substring
+ *
+ * Return: pointer to the byte in s that matches bytes in accept/ NULL if nil
+ */
+char * _strpbrk(char *s, char *accept)
+{
+	int i;
+	int len = strlen(accept);
+
+	while (*s)
+	{
+		for (i = 0; i < len; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return (NULL);
+}
