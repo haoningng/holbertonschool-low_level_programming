cf0ab19b702c8c97f9cfdeb3efb22767013c18ab
haoningng <nick.nghn@gmail.com>
2023-10-26 05:03:43 +0000

    update file 0

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -8,7 +8,7 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 	char *new_s = s;
 	
 	i = 0;
@@ -16,6 +16,7 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		*s = b;
 		s++;
+		i++;
 	}
 	return (new_s);
 }
