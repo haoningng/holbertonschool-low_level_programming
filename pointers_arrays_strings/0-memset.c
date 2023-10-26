7da9092a7f56f37e543593300fb9d5f8571714b2
haoningng <nick.nghn@gmail.com>
2023-10-26 05:02:18 +0000

    add file 0

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+/**
+ * _memset - fills the first n bytes of memory with a constant byte b
+ * @s: the pointer to the memory area
+ * @b: the constant byte
+ * @n: the first number of bytes of memory to fill
+ *
+ * Return: pointer to the memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+	char *new_s = s;
+	
+	i = 0;
+	while (i < n)
+	{
+		*s = b;
+		s++;
+	}
+	return (new_s);
+}
