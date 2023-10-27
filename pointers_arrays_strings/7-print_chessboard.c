6ff7ed5ae34f3efe7af089b88b4ea92731ad9f28
haoningng <nick.nghn@gmail.com>
2023-10-27 01:01:27 +0000

    add file 7

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: the 2D array
+ *
+ * Return: Nothing
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = j = 0;
+	for (i = 0; i < 8, i++)
+	{
+		for (j = 0; j < 8, j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
