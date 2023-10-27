a19c0196b860fc0f3825848c0a5b1b43527b51eb
haoningng <nick.nghn@gmail.com>
2023-10-27 01:02:23 +0000

    update file 7

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -11,9 +11,9 @@ void print_chessboard(char (*a)[8])
 	int i, j;
 
 	i = j = 0;
-	for (i = 0; i < 8, i++)
+	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; j < 8, j++)
+		for (j = 0; j < 8; j++)
 		{
 			printf("%c", a[i][j]);
 		}
