df73a7a511cdc79ab48ac0f1dc8d1d94f1c3ca2c
haoningng <nick.nghn@gmail.com>
2023-10-27 01:40:53 +0000

    update file 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -9,13 +9,17 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
 		if (*s == c)
 		{
-			return ((char *)s);
+			return (s);
 		}
 		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
 	}
 	return (NULL);
 }
