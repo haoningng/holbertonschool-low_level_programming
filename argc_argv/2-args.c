381be4a7e57ea1ac34b5428e6e6349acca21e770
haoningng <nick.nghn@gmail.com>
2023-10-30 22:29:00 -0700

    add file 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: count of the arguments supplied to the program
+ * @argv: an array of pointers to the strings which are those arguments
+ *
+ * Return: 0 if successful.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	(void)argv;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
