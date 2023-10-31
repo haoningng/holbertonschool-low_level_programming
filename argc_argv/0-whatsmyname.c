9dc2804e1f0303a89c2a501643d0fcffd777c02b
haoningng <nick.nghn@gmail.com>
2023-10-30 21:23:08 -0700

    add file 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mynameis - prints the program's name, followed by a new line.
+ * @argc: count of the arguments supplied to the program
+ * @argv: an array of pointers to the strings which are those arguments
+ *
+ * Return: Nothing.
+ */
+void mynameis(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+}
