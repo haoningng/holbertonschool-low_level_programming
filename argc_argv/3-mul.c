1e6a3e393a7e7faaaf43c19380fd485d9f587c19
haoningng <nick.nghn@gmail.com>
2023-10-30 22:36:31 -0700

    add stdlib library and atoi() in file 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -17,7 +18,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	i = argv[1] * argv[2];
+	i = atoi(argv[1]) * atoi(argv[2]);
 	printf("%d\n", i);
 	return (0);
 }
