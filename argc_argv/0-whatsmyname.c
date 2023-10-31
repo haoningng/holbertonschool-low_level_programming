cd06f6cba6d855aa250c9a8debbad46e7eef75e4
haoningng <nick.nghn@gmail.com>
2023-10-30 22:19:54 -0700

    change program name to main

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * mynameis - prints the program's name, followed by a new line
+ * main - prints the program's name, followed by a new line
  * @argc: count of the arguments supplied to the program
  * @argv: an array of pointers to the strings which are those arguments
  *
  * Return: Nothing.
  */
-void mynameis(int argc, char *argv[])
+void main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s", argv[0]);
+	printf("%s\n", argv[0]);
 }
