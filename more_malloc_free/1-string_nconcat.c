d0af7481adc95bfa4f7faeb0b4c68a7bab6a91aa
haoningng <nick.nghn@gmail.com>
2023-11-05 22:38:14 -0800

    remove square brackets to limit to 40 lines to make betty linter happy

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -13,47 +13,28 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
 	char *array;
-	int size1, size2;
+	int i, size1, size2;
 
 	if (s1 == NULL && s2 == NULL)
-	{
 		s1 = "";
 		s2 = "";
-	}
 	else if (s1 == NULL)
-	{
 		s1 = "";
-	}
 	else if (s2 == NULL)
-	{
 		s2 = "";
-	}
-	
 	if (n >= strlen(s2))
-	{
 		size2 = strlen(s2);
-	} 
 	else
-	{
 		size2 = n;
-	}
 	size1 = strlen(s1);
 	array = malloc((size1 + size2) * sizeof(char) + 1);
 	if (array == NULL)
-	{
 		return (NULL);
-	}
-
 	for (i = 0; i < size1; i++)
-	{
 		array[i] = s1[i];
-	}
 	for (i = 0; i < size2; i++)
-	{
 		array[i + size1] = s2[i];
-	}
 	array[size1 + size2] = '\0';
 	return (array);
 }
