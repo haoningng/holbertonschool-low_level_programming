e16f8b73b72a0693cb3e14fdf26ab1574a7ddb71
haoningng <nick.nghn@gmail.com>
2023-11-13 13:36:35 -0800

    check if d is NULL

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -3,13 +3,16 @@
 
 /**
  * free_dog - frees dogs
- * @d - pointer to the struct
+ * @d: pointer to the struct
  *
  * Return: Nothing
  */
 void free_dog(dog_t *d)
 {
-	free(d->name);
-	free(d->owner);
-	free(d);
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
