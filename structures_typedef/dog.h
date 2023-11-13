2bff31e80c25870f9235c122e88f4fd629a81a34
haoningng <nick.nghn@gmail.com>
2023-11-13 11:38:35 -0800

    add header file and README.md

---
+++ structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - this represents a dog
+ * @name: string representing its name
+ * @age: float representing its age
+ * @owner: string representing its owner's name
+ *
+ * Description: A dog structure will have name, age and owner name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
