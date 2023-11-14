758eaeb43d4f1c1c41dc58cad4f3154ac61ef774
haoningng <nick.nghn@gmail.com>
2023-11-14 01:36:26 -0800

    add stddef.h to function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,6 +2,8 @@
 
 #define HEADER_FILE
 
+#include <stddef.h>
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
