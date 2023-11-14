fee91d67f601b41a3d985ee46c1058897f2b53a2
haoningng <nick.nghn@gmail.com>
2023-11-13 19:34:05 -0800

    add file 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - executes a function given as a paraneter on an array
+ * @array: an array to be executed upon
+ * @size: size of the array
+ * @action: pointer to the function to be used
+ *
+ * Return: Nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i > size; i++)
+	{
+		action(array[i]);
+	}
+}
