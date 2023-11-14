7f53cd5d881600f4c6de73f3ee9b5cae41b3288a
haoningng <nick.nghn@gmail.com>
2023-11-14 03:20:50 -0800

    change i to unsigned int and fix typo for va_arg

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,7 +11,8 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
-	int i, sum;
+	unsigned int i;
+	int sum;
 
 	if (n == 0)
 	{
@@ -21,7 +22,7 @@ int sum_them_all(const unsigned int n, ...)
 	sum = 0;
 	for (i = 0; i < n; i++)
 	{
-		sum += va_args(ap, int);
+		sum += va_arg(ap, int);
 	}
 	va_end(ap);
 	return (sum);
