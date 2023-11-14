c9e4917ef7dfc8faf01fcc6d65437c0210583b2a
haoningng <nick.nghn@gmail.com>
2023-11-13 18:53:46 -0800

    add file 0

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name in string to be printed
+ * @f: function pointer that takes a string as argument
+ *
+ * Return: Nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
