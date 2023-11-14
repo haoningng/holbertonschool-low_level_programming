e3f01961f6ae94eec7c39b9cf2a64b71d7d79092
haoningng <nick.nghn@gmail.com>
2023-11-14 03:36:06 -0800

    use separator instead of hardcoding comma

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		element = va_arg(ap, int);
 		if (i != (n - 1))
 		{
-			printf("%d, ", element);
+			printf("%d%s ", element, separator);
 		}
 		else
 		{
