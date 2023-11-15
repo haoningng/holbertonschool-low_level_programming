f212bb1f27584571c4db876f3bc18afdfe6bf3aa
haoningng <nick.nghn@gmail.com>
2023-11-15 00:28:03 -0800

    check if argument to print_all is NULL

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -72,7 +72,7 @@ void print_all(const char * const format, ...)
 	};
 	va_start(args, format);
 	i = 0;
-	while (format[i] && format)
+	while (format && format[i])
 	{
 		j = 0;
 		while (arg_formats[j].format_type)
