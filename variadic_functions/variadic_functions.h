0a2faf21b706996e159cafe9b887862b9f8b66a6
haoningng <nick.nghn@gmail.com>
2023-11-15 00:20:26 -0800

    add typedef struct and prototypes for functions

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -2,6 +2,26 @@
 
 #define HEADER_FILE
 
+#include <stdarg.h>
+
+/**
+ * struct print_type - Struct print_type
+ *
+ * @format_type: the character representating different format type
+ * @f: the function pointer to print different format type
+ */
+typedef struct print_type
+{
+	char format_type;
+	void (*f)(va_list args);
+} print_type;
+
+/* These are the different functions to print different format type */
+void print_char(va_list args);
+void print_int(va_list args);
+void print_float(va_list args);
+void print_str(va_list args);
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
