9c665854c84f5889eefc1cd1fffa99cd182ea47d
haoningng <nick.nghn@gmail.com>
2023-11-26 19:39:55 -0800

    add file 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: the pointer to the list_t list
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	ptr = head->next;
+	while (ptr != NULL)
+	{
+		free(head->str);
+		free(head);
+		head = ptr;
+		ptr = ptr->next;
+	}
+	free(head->str);
+	free(head);
+}
