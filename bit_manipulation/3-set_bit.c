26dcc7dfba901da95fb6fc87999c9f811760599b
haoningng <nick.nghn@gmail.com>
2023-11-27 19:47:05 -0800

    add file 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * set_bit - set the value of a bit to 1 at a given index
+ * @n: number to be searched through
+ * @index: index of the bit of which value to be set to 1
+ *
+ * Return: 1 if it worked, -1 if error occured
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bitmask;
+
+	/* sizeof unsigned long int (bytes) x 8 bits */
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	bitmask = 1 << index;
+	*n = *n | bitmask;
+	return (1);
+}
