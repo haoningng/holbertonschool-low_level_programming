cd74d6dcb40de15aa9613181ea44d7f4d6a71936
haoningng <nick.nghn@gmail.com>
2023-11-27 19:51:52 -0800

    remove white space after ~

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -18,7 +18,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	bitmask = 1 << index;
-	bitmask = ~ bitmask;
+	bitmask = ~bitmask;
 	*n = *n & bitmask;
 	return (1);
 }
