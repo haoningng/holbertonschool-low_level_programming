a50ef400ac1f960abdd1c443f221faf6a02b52c8
haoningng <nick.nghn@gmail.com>
2023-12-04 15:56:07 -0800

    malloc for array

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,6 +15,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
+	new_table->array = malloc(size * 8);
+	if (new_table == NULL)
+		return (NULL);
 	new_table->size = size;
 	return (new_table);
 }
