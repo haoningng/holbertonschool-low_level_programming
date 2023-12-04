e10c59883a71ba5b717126819c0725907683f91e
haoningng <nick.nghn@gmail.com>
2023-12-04 15:02:26 -0800

    restructure the code

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -19,6 +19,7 @@ int main(int argc, char **argv)
 {
 	int fd, fd1, cnt, res, READ_SIZE = 1024;
 	char buffer[1024];
+	char *r = "Error: Can't read from file %s\n", *c = "Error: Can't close fd ";
 
 	if (argc != 3)
 	{
@@ -28,14 +29,14 @@ int main(int argc, char **argv)
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDOUT_FILENO, r, argv[1]);
 		exit(98);
 	}
 	fd1 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, r, argv[2]);
+		exit(98);
 	}
 	cnt = read(fd, buffer, READ_SIZE);
 	while (cnt != 0)
@@ -48,16 +49,10 @@ int main(int argc, char **argv)
 		}
 		cnt = read(fd, buffer, READ_SIZE);
 	}
-	if (cnt == -1)
+	if (cnt == -1 || close(fd) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, cnt == -1 ? r : c, argv[1]);
+		exit(cnt == -1 ? 98 : 100);
 	}
-	if (close(fd) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd);
-		exit(100);
-	}
-	close(fd1);
 	return (0);
 }
