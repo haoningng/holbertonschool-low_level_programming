0fb16b4901205f0a7187bb4f463beff76fcdbe45
haoningng <nick.nghn@gmail.com>
2023-12-05 13:40:38 +1100

    iterate through the existing array and update the value if keys matching

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "hash_tables.h"
@@ -17,10 +18,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	char *key_copy = strdup((char *)key), *value_copy = strdup((char *)value);
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
-	if (key == NULL)
-		return (0);
+	index = key_index((const unsigned char *)key_copy, ht->size);
+	new_node = ht->array[index];
+	while (new_node != NULL)
+	{
+		if (strcmp(new_node->key, key) == 0)
+		{
+			free(new_node->value);
+			new_node->value = value_copy;
+			return (1);
+		}
+		new_node = new_node->next;
+	}
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
@@ -39,8 +50,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	new_node->key = key_copy;
 	new_node->value = value_copy;
-	index = key_index((const unsigned char *)key_copy, ht->size);
-	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
 }
