03974402b0a9b4f329373e8590a2802245d8a4d4
haoningng <nick.nghn@gmail.com>
2023-12-04 20:41:24 -0800

    remove more lines

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -34,21 +34,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = malloc(sizeof(char) * strlen(key) + 1);
+	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-	new_node->value = malloc(sizeof(char) * strlen(value) + 1);
+	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
 		free(new_node->key);
 		free(new_node);
 		return (0);
 	}
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 	return (1);
