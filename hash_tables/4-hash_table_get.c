7bbdbe3f2da3aa194b9e012b6d34d965b31d3244
haoningng <nick.nghn@gmail.com>
2023-12-05 14:17:50 +1100

    iterate through exising array to find matching key before returning the value

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -8,15 +8,25 @@
  * hash_table_get - retrieves a value associated with a key
  * @ht: hash table we want to look into
  * @key: the key we are looking for
- * 
+ *
  * Return: value associated with the element/ NULL if key not found
 */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-    unsigned long int index;
+	unsigned long int index;
+	hash_node_t *node_pointer;
 
-    index = key_index((const unsigned char *)key, ht->size);
-    if (ht->array[index] == NULL)
-        return ("(null)");
-    return (ht->array[index]->value);
+	if (ht == NULL)
+		return ("(null)");
+	index = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[index] == NULL)
+		return ("(null)");
+	node_pointer = ht->array[index];
+	while (node_pointer != NULL)
+	{
+		if (strcmp(node_pointer->key, key) == 0)
+			return (node_pointer->value);
+		node_pointer = node_pointer->next;
+	}
+	return (ht->array[index]->value);
 }
